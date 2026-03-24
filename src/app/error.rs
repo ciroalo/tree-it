d1f383d574ce55720ead15aff068f1cccaa285c7
ciroalo <ciroalo2000@gmail.com>
2026-03-24 21:19:47 +0100

    Added app errors

---
+++ src/app/error.rs
@@ -0,0 +1,61 @@
+use crate::config::error::ConfigError;
+use crate::ignore::planner::PlanningError;
+use crate::matcher::error::MatcherError;
+use crate::render::error::RenderError;
+use crate::shared::path::PathError;
+use crate::fs::walker::WalkerError;
+
+#[derive(Debug)]
+pub enum AppError {
+    InvalidArguments(String),
+    InvalidTargetPath(String),
+    Io(std::io::Error),
+    Config(ConfigError),
+    Planning(PlanningError),
+    Matcher(MatcherError),
+    Walker(WalkerError),
+    Render(RenderError),
+    Path(PathError),
+}
+
+impl From<std::io::Error> for AppError {
+    fn from(value: std::io::Error) -> Self {
+        Self::Io(value)
+    }
+}
+
+impl From<ConfigError> for AppError {
+    fn from(value: ConfigError) -> Self {
+        Self::Config(value)
+    }
+}
+
+impl From<PlanningError> for AppError {
+    fn from(value: PlanningError) -> Self {
+        Self::Planning(value)
+    }
+}
+
+impl From<MatcherError> for AppError {
+    fn from(value: MatcherError) -> Self {
+        Self::Matcher(value)
+    }
+}
+
+impl From<WalkerError> for AppError {
+    fn from(value: WalkerError) -> Self {
+        Self::Walker(value)
+    }
+}
+
+impl From<RenderError> for AppError {
+    fn from(value: RenderError) -> Self {
+        Self::Render(value)
+    }
+}
+
+impl From<PathError> for AppError {
+    fn from(value: PathError) -> Self {
+        Self::Path(value)
+    }
+}
