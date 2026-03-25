78c024f6852295f4db93e8a20dc6d0163e0808ca
ciroalo <ciroalo2000@gmail.com>
2026-03-25 22:41:20 +0100

    Added tests common fixtures

---
+++ tests/common/fixture.rs
@@ -0,0 +1,20 @@
+use std::fs;
+use std::path::PathBuf;
+use std::time::{SystemTime, UNIX_EPOCH};
+
+pub fn create_temp_dir(prefix: &str) -> PathBuf{
+    let unique = SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .unwrap()
+        .as_nanos();
+
+    let dir = std::env::temp_dir().join(format!("{prefix}_{unique}"));
+    fs::create_dir_all(&dir).unwrap();
+    dir
+}
+
+pub fn cleanup_temp_dir(dir: &PathBuf) {
+    if dir.exists() {
+        fs::remove_dir_all(dir).unwrap();
+    }
+}
